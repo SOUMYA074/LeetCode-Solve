c3b82fd99815fc2fee32d67547e7b39d0be18519
Soumya Ranjan Sahoo <132675385+SOUMYA074@users.noreply.github.com>
2026-08-02 21:55:43 +0530

    Time: 383 ms (62.4%), Space: 0B (100%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,16 +1,17 @@
 WITH cte AS (
     SELECT *,
-           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+           LAG(id,1) OVER(ORDER BY id) AS prev1,
+           LAG(id,2) OVER(ORDER BY id) AS prev2,
+           LEAD(id,1) OVER(ORDER BY id) AS next1,
+           LEAD(id,2) OVER(ORDER BY id) AS next2
     FROM Stadium
     WHERE people >= 100
 )
 
 SELECT id, visit_date, people
 FROM cte
-WHERE grp IN (
-    SELECT grp
-    FROM cte
-    GROUP BY grp
-    HAVING COUNT(*) >= 3
-)
+WHERE
+    (id = prev1 + 1 AND prev1 = prev2 + 1)
+ OR (id = prev1 + 1 AND next1 = id + 1)
+ OR (next1 = id + 1 AND next2 = next1 + 1)
 ORDER BY visit_date;
