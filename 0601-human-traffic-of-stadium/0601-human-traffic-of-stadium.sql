1995083d066e702bbae5b5eb87b2af85e7d0c5e8
Soumya Ranjan Sahoo <132675385+SOUMYA074@users.noreply.github.com>
2026-08-02 21:54:54 +0530

    Time: 479 ms (11.69%), Space: 0B (100%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,30 +1,16 @@
-# Write your MySQL query statement below
-SELECT DISTINCT s.*
-FROM Stadium s
-JOIN Stadium s1
-JOIN Stadium s2
-WHERE
-(
-    s.id = s1.id - 1
-    AND s1.id = s2.id - 1
-    AND s.people >= 100
-    AND s1.people >= 100
-    AND s2.people >= 100
+WITH cte AS (
+    SELECT *,
+           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
 )
-OR
-(
-    s.id = s1.id + 1
-    AND s.id = s2.id - 1
-    AND s.people >= 100
-    AND s1.people >= 100
-    AND s2.people >= 100
+
+SELECT id, visit_date, people
+FROM cte
+WHERE grp IN (
+    SELECT grp
+    FROM cte
+    GROUP BY grp
+    HAVING COUNT(*) >= 3
 )
-OR
-(
-    s.id = s1.id + 2
-    AND s.id = s2.id + 1
-    AND s.people >= 100
-    AND s1.people >= 100
-    AND s2.people >= 100
-)
-ORDER BY s.visit_date;
+ORDER BY visit_date;
